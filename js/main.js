e708034babce9dec382ce6f28c35a5ada86784a4
Toby Carslake <toby.carslake@gmail.com>
2020-04-23 14:52:22 +0930

    finished lesson2 and set up sass eslint jasmine

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-(function() {
-
-	foo = 1;
-
-})();
+/* eslint-disable linebreak-style */
+// (function () {
+//   const foo = 1;
+//   return foo;
+// eslint-disable-next-line linebreak-style
+// }());
